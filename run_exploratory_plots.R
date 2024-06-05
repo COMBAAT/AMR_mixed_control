552736bb919470f18acc5aa4fd693838dce5af5e
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-05 13:28:15 +0100

    scenarios contained in dataframe scenarios_df - simplifies loops - simplifies outputs by using this_scenario to append parameters to outputs

--- run_exploratory_plots.R
+++ run_exploratory_plots.R
@@ -17,40 +17,10 @@ source("funcs/r0.R")
 source("funcs/set1_params_inits.R")
 source("funcs/qual_check.R")
 source("funcs/quick_plot.R")
+source("input_scenarios.R")
 
 loops <- TRUE
-
-
-if (loops == TRUE) {
-  max_time <- 5000
-  treatment_type = "F"  #F this means quick treatment
-  N_wl <- c(0, 100, 250)
-  treat.prop.vecA <- seq(0, 0.9, by = 0.2)      #full from 0-1
-  treat.prop.vecB <- seq(0.91, 0.99, by = 0.02)
-  treat.prop.vec <- c(treat.prop.vecA, treat.prop.vecB)
-  K.vec <- c(10000, 6000, 2000, 1000, 500)
-  fit.adj.vec <- c(0.95)
-  birth.adj.vec <- c(2)
-  # do not set prop.insecticide to 1 as generates infinite mortality and an error
-  prop.insecticide.vec <- c(0.0,0.025, 0.05, 0.10,0.15, 0.2, 0.3, 0.5, 0.8, 0.99)
-  prop.prophylaxis <- 0.0
-  dose.adj.vec <- 1#c(1, 0.9, 0.7, 0.4, 0.2)
-  emergence.adj.vec <- 1
-} else {
-  max_time <- 5000
-  treatment_type = "F" 
-  N_wl <- 250
-  treat.prop.vec <- 0.99
-  #equil.vector_pop <- c(3000)
-  fit.adj.vec <- c(0.6)
-  K.vec <- 2000
-  prop.insecticide.vec <- 0.0
-  birth.adj.vec <- 2
-  prop.prophylaxis <- 0.5
-  dose.adj.vec <- 0.1
-  emergence.adj.vec <- 1
-}
-
+scenarios_df <- input_scenarios(loops)
 
 df <-data.frame(CS = c(), total.cattle = c(), treat_prop = c(), W_st = c(), R_eq_sen = c(), 
                 R0_sen = c(), R_eq_res = c(), R0_res = c(), No_trt_cat = c(), 
@@ -63,40 +33,26 @@ df2 <- data.frame()
 ## ---- Run time estimates
 tic()
 start.time <- Sys.time()
-total_steps <- length(birth.adj.vec) * length(prop.insecticide.vec) * length(fit.adj.vec) * length(K.vec) * length(N_wl) * length(treat.prop.vec) * length(dose.adj.vec) * length(emergence.adj.vec)
-step_started <- 0
-progress_floor <- 0
 
 ## ---- Execute model
 
-for (birth.adj in birth.adj.vec) {
-  for (prop.insecticide in prop.insecticide.vec) {
-    for (fit.adj in fit.adj.vec) {
-      for (K in K.vec) {
-        for (NW in N_wl) {
-          for (prop_treat in treat.prop.vec) {
-            for (dose.adj in dose.adj.vec){
-              for(emergence.adj in emergence.adj.vec){
-                step_started <- step_started + 1
-                progress_old <- progress_floor
-                progress <- step_started/total_steps*100
-                progress_floor <- floor(progress)
-                if (progress_floor == progress_old + 1){
-                  current.time <- Sys.time()
-                  time_taken_minutes <- as.numeric(difftime(current.time, start.time, units = "mins"))
-                  time_remaining <- 100*time_taken_minutes/progress - time_taken_minutes
-                  cat(red("% Complete = ", round(progress,2), "Time elapsed =", round(time_taken_minutes,2), 
-                          "mins", "Time left =", round(time_remaining,2), "\n"))}
+for (i in 1:nrow(scenarios_df)) {
+  print(i)
+  
+                this_scenario <- scenarios_df[i, ]
                 
-                
-                params <- set1(output = "P", birth.adj = birth.adj, fit.adj = fit.adj, 
-                               K = K, prop_treat = prop_treat, prop.insecticide = prop.insecticide, 
-                               NW = NW, prop.prophylaxis = prop.prophylaxis, treatment_type, 
-                               dose.adj = dose.adj, emergence.adj = emergence.adj)
-                inits <- set1(output = "I", birth.adj = birth.adj, fit.adj = fit.adj, 
-                              K = K, prop_treat = prop_treat, prop.insecticide = prop.insecticide, 
-                              NW = NW, prop.prophylaxis = prop.prophylaxis, treatment_type, 
-                              dose.adj = dose.adj, emergence.adj = emergence.adj)
+                params <- set1(output = "P", birth.adj = this_scenario$birth.adj, fit.adj = this_scenario$fit.adj, 
+                               K = this_scenario$K, treat_prop = this_scenario$treat_prop, 
+                               prop.insecticide = this_scenario$prop.insecticide, 
+                               NW = this_scenario$NW, prop.prophylaxis = this_scenario$prop.prophylaxis, 
+                               trt.type = this_scenario$treatment_type, 
+                               dose.adj = this_scenario$dose.adj, emergence.adj = this_scenario$emergence.adj)
+                inits <- set1(output = "I", birth.adj = this_scenario$birth.adj, fit.adj = this_scenario$fit.adj, 
+                              K = this_scenario$K, treat_prop = this_scenario$treat_prop, 
+                              prop.insecticide = this_scenario$prop.insecticide, 
+                              NW = this_scenario$NW, prop.prophylaxis = this_scenario$prop.prophylaxis, 
+                              trt.type = this_scenario$treatment_type, 
+                              dose.adj = this_scenario$dose.adj, emergence.adj = this_scenario$emergence.adj)
                 
                 qual_check_no0(params) # ensure there are no negative values
                 qual_check_no0(inits) # ensure there are no negative values
@@ -124,7 +80,7 @@ for (birth.adj in birth.adj.vec) {
                 
                 
                 ## Times ----
-                times <- seq(0, max_time, 1)
+                times <- seq(0, this_scenario$max_time, 1)
                 
                 ## RUN MODEL ----
                 #out <-ode(y = inits, parms = params, func = AAT_AMR_dens_dep, times = times, method = "daspk",
@@ -177,7 +133,7 @@ for (birth.adj in birth.adj.vec) {
                 #be mindful of prophylactic treatment that they can be re-treated
                 #2 stage run - pull calcs out, check for quick trt, expand to prophylactic next
                 
-                if(treatment_type == "F"){
+                if(this_scenario$treatment_type == "F"){
                   No.trt.cat <-  params["treatment.q"] * last$CIs * 365.25
                   Inc <- params["gamma.c"] * last$CEs * 365.25
                   P.onward.trans <- 1 - dpois(0, Rres[1])
@@ -185,7 +141,7 @@ for (birth.adj in birth.adj.vec) {
                   riskE <- (1 - dpois(0, Rres[1])) * ( last$CTs + last$PTs)                  
                 }
                 
-                if(treatment_type == "P"){
+                if(this_scenario$treatment_type == "P"){
                   No.trt.cat <-  params["treatment.p"] * (last$PIs + last$CIs) * 365.25
                   Inc <- params["gamma.c"] * (last$PEs + last$CEs) * 365.25
                   P.onward.trans <- 1 - dpois(0, Rres[1])
@@ -193,7 +149,7 @@ for (birth.adj in birth.adj.vec) {
                   riskE <- (1 - dpois(0, Rres[1])) * (last$PEs + last$PIs + last$PPs)
                 }
                 
-                if(treatment_type == "B"){
+                if(this_scenario$treatment_type == "B"){
                   No.trt.cat <-  (params["treatment.p"]+ params["treatment.q"]) * (last$PIs + last$CIs) * 365.25
                   Inc <- params["gamma.c"] * (last$PEs + last$CEs) * 365.25 
                   P.onward.trans <- 1 - dpois(0, Rres[1])
@@ -205,17 +161,16 @@ for (birth.adj in birth.adj.vec) {
                 prev <- (last$PIs + last$CIs) / last$all.cows
                 
                 
-                selected_outputs <- data.frame(W_st = out[1, "WS"], R_eq_sen = Rsen[1], R0_sen = R0s[1], 
+                selected_outputs <- cbind( data.frame(W_st = out[1, "WS"], R_eq_sen = Rsen[1], R0_sen = R0s[1], 
                                                R_eq_res = Rres[1], R0_res = R0r[1], 
                                                No_trt_cat = No.trt.cat, 
                                                Incidence = Inc, 
                                                Vector_no = as.numeric(inits["VSt"]), Prob_onward_tran = P.onward.trans, 
                                                RiskA = riskA , RiskE = riskE,
                                                prevalence = prev, vector_birth = params["birth.v"], 
-                                               vector_mortality = params["death.v"], fit.adj = params["fit.adj"], 
-                                               prop.insecticide = prop.insecticide, birth.adj = birth.adj,
-                                               eq_pop = params["equil.vector_pop"], K = params["K"], treat_prop = prop_treat,
-                                               dose = dose.adj, emergence = emergence.adj)
+                                               vector_mortality = params["death.v"], 
+                                               eq_pop = params["equil.vector_pop"],
+                                               this_scenario))
                 df = rbind(df, selected_outputs)
                 
                 wide <- cbind(selected_outputs, last)
@@ -223,13 +178,7 @@ for (birth.adj in birth.adj.vec) {
                 
                 
                 print(last$times)
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+ 
 }  
 
 toc()
@@ -241,7 +190,7 @@ Rres
 
 test <- df2
 time <- format(Sys.time(), "%a %b %d %X %Y")
-save(test, file = paste0("output/test_", treatment_type, ".Rda"))
+save(test, file = paste0("output/test_", this_scenario$treatment_type, "_play", ".Rda"))
 #save(test,file ="output/test.Rda")
 
 quick_plot(out)
