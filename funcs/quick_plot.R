da4f07d6bfb4c6ca5251e7296adecf6c0a4545ed
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-06 22:18:29 +0100

    two new functions added using ggplot rather thn base R

--- funcs/quick_plot.R
+++ funcs/quick_plot.R
@@ -1,3 +1,12 @@
+library(ggplot2)
+library(gghighlight)
+library(dplyr)
+library(patchwork)
+library(tidyr)
+
+# Source files and function
+source("funcs/plot_helper.R")
+
 quick_plot <- function(ODEinput){
   
   
@@ -57,3 +66,177 @@ quick_plot <- function(ODEinput){
   #  €   y = c("VSt","VSf", "VEs", "VEr", "VIs", "VIr"), pch = 15)
   
 }
+
+
+
+quick_plot2 <- function(df) {
+  ggplot(df) +
+    geom_line(aes(x = times, y = CS), colour = "blue") +
+    geom_line(aes(x = times, y = CEs), colour = "orange") +
+    geom_line(aes(x = times, y = CEr), colour = "orange") +
+    geom_line(aes(x = times, y = CIs), colour = "red") +
+    geom_line(aes(x = times, y = CIr), colour = "red") +
+    geom_line(aes(x = times, y = CTs), colour = "green") +
+    geom_line(aes(x = times, y = CTr), colour = "green") +
+    ylab("Number") +
+    xlab("Time, days") +
+    ggtitle("Cattle, no prophylaxis")
+
+  this_linewidth <- 2
+  p1 <- ggplot(df) +
+    geom_line(aes(x = times, y = CS, colour = "Sus"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CEs, colour = "Exp s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CEr, colour = "Exp r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CIs, colour = "Inf s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CIr, colour = "Inf r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CTs, colour = "Trt s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = CTr, colour = "Trt r"), linewidth = this_linewidth) +
+    ylab("Number") +
+    xlab("Time (days)") +
+    ggtitle("Cattle, no prophylaxis") +
+    scale_color_manual("Status",
+      values = c(
+        "Full" = "purple",
+        "Partial" = "skyblue",
+        "Sus" = "blue",
+        "Exp s" = "orange",
+        "Exp r" = "darkorange",
+        "Inf s" = "red",
+        "Inf r" = "darkred",
+        "Trt s" = "green",
+        "Trt r" = "darkgreen"
+      )
+    )
+
+  p2 <- ggplot(df) +
+    geom_line(aes(x = times, y = PF, colour = "Full"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PS, colour = "Partial"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PEs, colour = "Exp s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PEr, colour = "Exp r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PIs, colour = "Inf s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PIr, colour = "Inf r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PTs, colour = "Trt s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = PTr, colour = "Trt r"), linewidth = this_linewidth) +
+    ylab("Number") +
+    xlab("Time (days)") +
+    ggtitle("Cattle, with prophylaxis") +
+    scale_color_manual("Status",
+      values = c(
+        "Full" = "purple",
+        "Partial" = "skyblue",
+        "Sus" = "blue",
+        "Exp s" = "orange",
+        "Exp r" = "darkorange",
+        "Inf s" = "red",
+        "Inf r" = "darkred",
+        "Trt s" = "green",
+        "Trt r" = "darkgreen"
+      )
+    )
+  
+  p3 <- ggplot(df) +
+    geom_line(aes(x = times, y = WS, colour = "Sus"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = WEs, colour = "Exp s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = WEr, colour = "Exp r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = WIs, colour = "Inf s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = WIr, colour = "Inf r"), linewidth = this_linewidth) +
+    ylab("Number") +
+    xlab("Time (days)") +
+    ggtitle("Wildlife") +
+    scale_color_manual("Status",
+                       values = c(
+                         "Full" = "purple",
+                         "Partial" = "skyblue",
+                         "Sus" = "blue",
+                         "Exp s" = "orange",
+                         "Exp r" = "darkorange",
+                         "Inf s" = "red",
+                         "Inf r" = "darkred",
+                         "Trt s" = "green",
+                         "Trt r" = "darkgreen"
+                       )
+    )
+  
+  p4 <- ggplot(df) +
+    geom_line(aes(x = times, y = VSf, colour = "Ten"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = VSt, colour = "Sus"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = VEs, colour = "Exp s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = VEr, colour = "Exp r"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = VIs, colour = "Inf s"), linewidth = this_linewidth) +
+    geom_line(aes(x = times, y = VIr, colour = "Inf r"), linewidth = this_linewidth) +
+    ylab("Number") +
+    xlab("Time (days)") +
+    ggtitle("Vector") +
+    scale_color_manual("Status",
+                       values = c(
+                         "Full" = "purple",
+                         "Partial" = "skyblue",
+                         "Sus" = "blue",
+                         "Exp s" = "orange",
+                         "Exp r" = "darkorange",
+                         "Inf s" = "red",
+                         "Inf r" = "darkred",
+                         "Trt s" = "green",
+                         "Trt r" = "darkgreen"
+                       )
+    )
+
+  p1 + p2 + p3 + p4 + plot_layout(nrow = 2)
+}
+
+
+
+quick_plot3 <- function(df) {
+  new_df <- pivot_longer(df, !times, names_to = "Status", values_to = "Number")
+  new_df <- new_df %>% mutate(Animal_type = str_sub(Status, 1, 1))
+
+  these_colours <- c(
+    "CS" = "blue", "CEs" = "orange", "CEr" = "orange",
+    "CIs" = "red", "CIr" = "red", "CTs" = "green", "CTr" = "green", 
+    "PF" = "purple", "PS" = "skyblue", "PEs" = "orange", "PEr" = "orange",
+    "PIs" = "red", "PIr" = "red", "PPs" = "grey", "PPr" = "grey", "PTs" = "green", "PTr" = "green",
+    "WS" = "blue", "WEs" = "orange", "WEr" = "orange", "WIs" = "red", "WIr" = "red",
+    "VSt" = "purple", "Vsf" = "blue", "VEs" = "orange", "VEr" = "orange", 
+    "VIs" = "red", "VIr" = "red"
+  )
+  plot_C <- new_df %>%
+    filter(Animal_type == "C") %>%
+    ggplot() +
+    geom_line(aes(x = times, y = Number, colour = Status), linewidth = 1.5) +
+    xlab("Time (days)") +
+    scale_color_manual(" ", values = these_colours) +
+    ggtitle("Cattle, no prophylaxis") +
+    my_theme()
+  
+  plot_P <- new_df %>%
+    filter(Animal_type == "P") %>%
+    ggplot() +
+    geom_line(aes(x = times, y = Number, colour = Status), linewidth = 1.5) +
+    xlab("Time (days)") +
+    scale_color_manual(" ", values = these_colours) +
+    ggtitle("Cattle with prophylaxis") +
+    my_theme()
+  
+  plot_W <- new_df %>%
+    filter(Animal_type == "W") %>%
+    ggplot() +
+    geom_line(aes(x = times, y = Number, colour = Status), linewidth = 1.5) +
+    xlab("Time (days)") +
+    scale_color_manual(" ", values = these_colours) +
+    ggtitle("Wildlife") +
+    my_theme()
+  
+  plot_V <- new_df %>%
+    filter(Animal_type == "V") %>%
+    ggplot() +
+    geom_line(aes(x = times, y = Number, colour = Status), linewidth = 1.5) +
+    xlab("Time (days)") +
+    scale_color_manual(" ", values = these_colours) +
+    ggtitle("Vector") +
+    my_theme()
+  
+  plot_C + plot_P + plot_W + plot_V
+}
+
+
+
