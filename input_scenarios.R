ab4ca42026f7b09d5ab6268a81b39fca95778413
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-11 10:45:49 +0100

    adjust scenario and delete commented out line setting equil.vector_pop

--- input_scenarios.R
+++ input_scenarios.R
@@ -2,7 +2,7 @@
 input_scenarios <- function(loops){
   
   if (loops == TRUE) {
-    max_time <- 5
+    max_time <- 3000
     treatment_type = "F"  #F this means quick treatment
     N_wl <- c(0, 100, 250)
     treat.prop.vecA <- seq(0, 0.9, by = 0.2)      #full from 0-1
@@ -12,7 +12,8 @@ input_scenarios <- function(loops){
     fit.adj.vec <- c(0.95)
     birth.adj.vec <- c(2)
     # do not set prop.insecticide to 1 as generates infinite mortality and an error
-    prop.insecticide.vec <- c(0.0,0.025, 0.05, 0.10,0.15, 0.2, 0.3, 0.5, 0.8, 0.99)
+    #prop.insecticide.vec <- c(0.0,0.025, 0.05, 0.10,0.15, 0.2, 0.3, 0.5, 0.8, 0.99)
+    prop.insecticide.vec <- c(0.0, 0.05, 0.10, 0.15, 0.2)
     prop.prophylaxis <- 0.0
     dose.adj.vec <- 1#c(1, 0.9, 0.7, 0.4, 0.2)
     emergence.adj.vec <- 1
@@ -20,14 +21,13 @@ input_scenarios <- function(loops){
     max_time <- 5000
     treatment_type = "F" 
     N_wl <- 250
-    treat.prop.vec <- 0.99
-    #equil.vector_pop <- c(3000)
-    fit.adj.vec <- c(0.6)
+    treat.prop.vec <- 0.25
+    fit.adj.vec <- c(0.95)
     K.vec <- 2000
     prop.insecticide.vec <- 0.0
     birth.adj.vec <- 2
-    prop.prophylaxis <- 0.5
-    dose.adj.vec <- 0.1
+    prop.prophylaxis <- 0.0
+    dose.adj.vec <- 1.0
     emergence.adj.vec <- 1
   }
   
