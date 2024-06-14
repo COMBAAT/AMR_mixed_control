4e8caa8c74fa603b66fac2d2c2a2a32643d7db97
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-14 17:00:40 +0100

    change maxtime to 10000, expand range of insecticide treatments

--- input_scenarios.R
+++ input_scenarios.R
@@ -2,7 +2,7 @@
 input_scenarios <- function(loops){
   
   if (loops == TRUE) {
-    max_time <- 5000
+    max_time <- 10000
     treatment_type = "F"  #F this means quick treatment
     N_wl <- c(0, 100, 250)
     treat.prop.vecA <- seq(0, 0.9, by = 0.2)      #full from 0-1
@@ -12,8 +12,8 @@ input_scenarios <- function(loops){
     fit.adj.vec <- c(0.95)
     birth.adj.vec <- c(2)
     # do not set prop.insecticide to 1 as generates infinite mortality and an error
-    #prop.insecticide.vec <- c(0.0,0.025, 0.05, 0.10,0.15, 0.2, 0.3, 0.5, 0.8, 0.99)
-    prop.insecticide.vec <- c(0.0, 0.05, 0.10, 0.15, 0.2)
+    #prop.insecticide.vec <- c(0.0, 0.05, 0.10, 0.15, 0.2)
+    prop.insecticide.vec <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.3, 0.4, 0.5)
     prop.prophylaxis <- 0.0
     dose.adj.vec <- 1#c(1, 0.9, 0.7, 0.4, 0.2)
     emergence.adj.vec <- 1
