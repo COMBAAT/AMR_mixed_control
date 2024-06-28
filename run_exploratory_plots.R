2998225caf7f4081ea5f9d6489f0aa6cd6a82c53
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-28 13:41:32 +0100

    some additional comments and removal of white space

--- run_exploratory_plots.R
+++ run_exploratory_plots.R
@@ -19,9 +19,13 @@ source("funcs/qual_check.R")
 source("funcs/quick_plot.R")
 source("input_scenarios.R")
 
-loops <- TRUE
-scenarios_df <- input_scenarios(loops)
+## ---- 
+# Specify multiple or single scenario
+multiple_scenarios <- TRUE
+# Create dataframe of parameter combinations for each scenario
+scenarios_df <- input_scenarios(multiple_scenarios)
 
+# Create empty dataframes to store outputs
 df <- data.frame()
 df2 <- data.frame()
 
@@ -30,50 +34,50 @@ tic()
 start.time <- Sys.time()
 
 ## ---- Execute model
-
-for (i in 1:nrow(scenarios_df)) {
-  print(i)
+for (row in 1:nrow(scenarios_df)) {
+  print(row)
   
-                this_scenario <- scenarios_df[i, ]
+                this_scenario <- scenarios_df[row, ]
                 
+                # Get params and intis for this scenario
                 params_and_inits <- set1(this_scenario)
                 params <- params_and_inits[["params"]]
                 inits <- params_and_inits[["inits"]]
                 
+                # Remove unused parameters and variables
                 myvars <- names(params) %in% c("resusceptible", "resusceptible.w") 
                 params <- params[!myvars]
                 
                 myvars2 <- names(inits) %in% c("CR", "PR", "WR") 
                 inits <- inits[!myvars2]
                 
-                
-                ## R0 calculations
+                ## R0 calculations - only run full simulation if R0 >= 1.0
                 R0sen_and_R0res <- calculate_R0_from_inits(inits)
                 R0s <- R0sen_and_R0res[[1]]
                 
-                ## Times ----
+                ## Set simulation times dependent on R0 value
                 if (R0s[1] < 1.0){
                   # if R0 < 1, set inits to disease free equilibrium and exit simulation after 0.1 day
                   inits['CS'] <- inits['CS'] + inits['CIs']
                   inits['CIs'] <- 0
                   times <- seq(0, 0.1, 0.1)
                 } else {
+                  # if R0 >= 1.0 run full simulation
                   times <- seq(0, this_scenario$max_time, 1)
                 }
                 
                 ## RUN MODEL ----
+                #out <-ode(y = inits, parms = params, func = AAT_AMR_dens_dep, times = times, method = "daspk")
+                # use rootfunc option to exist simulation when Rsen < 1.01 or Number infected cattle < 1e-5
                 out <-ode(y = inits, parms = params, func = AAT_AMR_dens_dep, times = times,
                           rootfunc = my_rootfun3, events = list(root = TRUE, terminalroot = c(1,2)))
-                #out <-ode(y = inits, parms = params, func = AAT_AMR_dens_dep, times = times, method = "daspk")
                 out <- as.data.frame(out)
                 names(out)[names(out) == 'time'] <- "times"
-                
                 
                 expanded_output <- add_totals(out)
                 expanded_output <- add_R0(inits, expanded_output)
                 expanded_output <- add_R_trajectories(params, expanded_output)
                 last <- tail(expanded_output, 1)
-                
                 
                 # #Check get 1 at equilibrium when run with only sensitive strains
                 # #fraction of cattle available for infection by sensitive strain
@@ -83,7 +87,6 @@ for (i in 1:nrow(scenarios_df)) {
                 # 
                 # #fraction of wildlife available for infection by sensitive strain
                 # if (Nw > 0) {fW = last$WS / Nw} else {fW <- 0}
-                
                 
                 ## R calculations
                 Rsen_and_Rres <- calculate_R_from_row_of_df(params, last)
@@ -131,9 +134,7 @@ for (i in 1:nrow(scenarios_df)) {
                   RiskE <- (1 - dpois(0, Rres[1])) * (last$PEs + last$PIs + last$PPs + last$CTs + last$PTs)
                 }
                 
-                
                 prev <- (last$PIs + last$CIs) / last$All.cows
-                
                 
                 selected_outputs <- cbind( data.frame(scenario_id = i, this_scenario, W_st = out[1, "WS"], 
                                                No_trt_cat = No_trt_cat, 
@@ -148,7 +149,6 @@ for (i in 1:nrow(scenarios_df)) {
                 
                 wide <- cbind(selected_outputs, last)
                 df2 = rbind(df2, wide)
-                
                 
                 print(last$times)
                 
