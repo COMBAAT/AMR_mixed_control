8115b1d5c89f61c806c0f41f297c0f05b69c8850
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-06-28 16:22:59 +0100

    moved root functions in ode_stopping_functions.R

---
+++ funcs/ode_stopping_functions.R
@@ -0,0 +1,24 @@
+my_rootfun <- function(t, y, params) {
+  return(c(y['CS'] - 20.0, y['CIs'] - 10.0))
+}
+
+my_rootfun2 <- function (t, y, params) {
+  dstate <- unlist(AAT_AMR_dens_dep(t, y, params)) # rate of change vector
+  condition1 <- (y['CIs'] - 1e-5)
+  condition2 <- sum(abs(dstate)) - 1e-5
+  return(c(condition1, condition2))
+}
+
+my_rootfun3 <- function (t, y, params) {
+  dstate <- unlist(AAT_AMR_dens_dep(t, y, params)) # rate of change vector
+  Nc <- y['CS']
+  Np <- y['PS']
+  Nw <- y['WS']
+  Nv <- y['VSt'] + y['VSf']
+  Rsen <- r0_calc_sen_or_res(params, Nc, Np, Nw, Nv, sen = "yes", basic = "no")[1]
+  
+  condition1 <- (Rsen - 1.01)
+  condition2 <- (y['CIs'] - 1e-5)
+  
+  return(c(condition1, condition2))
+}
--- funcs/set1_params_inits.R
+++ funcs/set1_params_inits.R
@@ -174,40 +174,7 @@ set1 <- function(this_scenario){
   
 }
 
-
-my_rootfun <- function(t, y, params) {
-  return(c(y['CS'] - 20.0, y['CIs'] - 10.0))
-}
-
-my_rootfun2 <- function (t, y, params) {
-  dstate <- unlist(AAT_AMR_dens_dep(t, y, params)) # rate of change vector
-  condition1 <- (y['CIs'] - 1e-5)
-  condition2 <- sum(abs(dstate)) - 1e-5
-  return(c(condition1, condition2))
-}
-
-my_rootfun3 <- function (t, y, params) {
-  dstate <- unlist(AAT_AMR_dens_dep(t, y, params)) # rate of change vector
-  Nc <- y['CS']
-  Np <- y['PS']
-  Nw <- y['WS']
-  Nv <- y['VSt'] + y['VSf']
-  Rsen <- r0_calc_sen_or_res(params, Nc, Np, Nw, Nv, sen = "yes", basic = "no")[1]
-  
-  condition1 <- (Rsen - 1.01)
-  condition2 <- (y['CIs'] - 1e-5)
-  
-  return(c(condition1, condition2))
-}
-
-
 findGlobals(fun = set1, merge = FALSE)$variables
 
-## Error Checks ----
-
-
-
-#if(CIs + CIr > NC){cat(red("WARNING: Infected cattle (", CIs + CIr, ") is GREATER THAN total cattle(", NC, ")\n"))}
-#if(WIs > NW){cat(red("WARNING: Infected wildlife (", WIs, ") is GREATER THAN total wildlife(", NW, ")\n"))}
 
 
