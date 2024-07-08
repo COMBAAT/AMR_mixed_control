0d1c484994e2cbb09f54074b3e841e0d907851ec
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-08 18:31:07 +0100

    renamed N_wl wildlife number; removed "_vec"; renamed K to carrying_capacity

--- input_scenarios.R
+++ input_scenarios.R
@@ -1,26 +1,27 @@
 
 create_multiple_scenarios <- function() {
-  max_time <- 10000
+  max_time <- 10
   treatment_type <- "F" # F this means quick treatment
-  N_wl <- c(0, 50, 100, 150, 200, 250)
-  treat_prop_vecA <- seq(0, 0.9, by = 0.2) # full from 0-1
-  treat_prop_vecB <- seq(0.91, 0.99, by = 0.02)
-  treat_prop_vec <- c(treat_prop_vecA, treat_prop_vecB)
-  K.vec <- c(10000, 6000, 2000, 1000, 500)
-  fit_adj_vec <- c(0.95)
-  birth_adj_vec <- c(2)
+  #wildlife_number <- c(0, 50, 100, 150, 200, 250)
+  wildlife_number <- c(0, 100, 250)
+  treat_propA <- seq(0, 0.9, by = 0.2) # full from 0-1
+  treat_propB <- seq(0.91, 0.99, by = 0.02)
+  treat_prop <- c(treat_propA, treat_propB)
+  carrying_capacity <- c(10000, 6000, 2000, 1000, 500)
+  fit_adj <- c(0.95)
+  birth_adj <- c(2)
   # do not set prop_insecticide to 1 as generates infinite mortality and an error
-  #prop_insecticide.vec <- c(0.0, 0.05, 0.10, 0.15, 0.2, 0.5)
-   prop_insecticide.vec <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
+  prop_insecticide <- c(0.0, 0.05, 0.10, 0.15, 0.2, 0.5)
+  #prop_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
   prop_prophylaxis <- 0.0
-  dose_adj_vec <- 1 # c(1, 0.9, 0.7, 0.4, 0.2)
-  emergence_adj_vec <- 1
+  dose_adj <- 1 # c(1, 0.9, 0.7, 0.4, 0.2)
+  emergence_adj <- 1
 
   df <- expand.grid(
-    emergence_adj = emergence_adj_vec, dose_adj = dose_adj_vec,
-    treat_prop = treat_prop_vec, NW = N_wl, K = K.vec,
-    fit_adj = fit_adj_vec, prop_insecticide = prop_insecticide.vec,
-    birth_adj = birth_adj_vec, prop_prophylaxis = prop_prophylaxis,
+    emergence_adj = emergence_adj, dose_adj = dose_adj,
+    treat_prop = treat_prop, NW = wildlife_number, K = carrying_capacity,
+    fit_adj = fit_adj, prop_insecticide = prop_insecticide,
+    birth_adj = birth_adj, prop_prophylaxis = prop_prophylaxis,
     treatment_type = treatment_type, max_time = max_time
   )
   df
@@ -29,21 +30,21 @@ create_multiple_scenarios <- function() {
 create_single_scenario <- function() {
   max_time <- 5000
   treatment_type <- "F"
-  N_wl <- 250
-  treat_prop_vec <- 0.25
-  fit_adj_vec <- c(0.95)
-  K.vec <- 2000
-  prop_insecticide.vec <- 0.0
-  birth_adj_vec <- 2
+  wildlife_number <- 250
+  treat_prop <- 0.25
+  fit_adj <- c(0.95)
+  carrying_capacity <- 2000
+  prop_insecticide <- 0.0
+  birth_adj <- 2
   prop_prophylaxis <- 0.0
-  dose_adj_vec <- 1.0
-  emergence_adj_vec <- 1
+  dose_adj <- 1.0
+  emergence_adj <- 1
 
   df <- data.frame(
-    emergence_adj = emergence_adj_vec, dose_adj = dose_adj_vec,
-    treat_prop = treat_prop_vec, NW = N_wl, K = K.vec,
-    fit_adj = fit_adj_vec, prop_insecticide = prop_insecticide.vec,
-    birth_adj = birth_adj_vec, prop_prophylaxis = prop_prophylaxis,
+    emergence_adj = emergence_adj, dose_adj = dose_adj,
+    treat_prop = treat_prop, NW = wildlife_number, K = carrying_capacity,
+    fit_adj = fit_adj, prop_insecticide = prop_insecticide,
+    birth_adj = birth_adj, prop_prophylaxis = prop_prophylaxis,
     treatment_type = treatment_type, max_time = max_time
   )
   df
