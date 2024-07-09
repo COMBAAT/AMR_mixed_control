1244b087f7c965c0ea280c277aab2644a251f1bd
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-09 10:42:10 +0100

    changed spelling from _infectious_period to _infection_period for cattle and wildlife; added set_parameters_NEW

--- funcs/set_params_and_inits.R
+++ funcs/set_params_and_inits.R
@@ -5,17 +5,19 @@ library(codetools)
 ## Set parameters & Initial Conditions ----
 
 
+
+
 set_baseline_parameters <- function() {
   # Using day as unit of time
   cattle_lifespan <- 5 * 365
   cattle_incubation_period <- 15
-  cattle_infectious_period <- 100
+  cattle_infection_period <- 100
   cattle_proph_full_protection_period <- 60
   cattle_proph_partial_protection_period <- 30
   cattle_treatment_period <- 3
 
   wildlife_lifespan <- 365
-  wildlife_infectious_period <- cattle_infection_period
+  wildlife_infection_period <- cattle_infection_period
   wildlife_incubation_period <- 20
 
   bite_rate <- 0.8 / 4
@@ -26,9 +28,9 @@ set_baseline_parameters <- function() {
   prob_vector_surviving_feed <- 0.96
   prob_vector_surviving_nonfeeding_day <- 0.98
   
-  baseline_params <- cbind(cattle_lifespan, cattle_incubation_period, cattle_infectious_period,
+  baseline_params <- cbind(cattle_lifespan, cattle_incubation_period, cattle_infection_period,
                            cattle_proph_full_protection_period, cattle_proph_partial_protection_period,
-                           cattle_treatment_period, wildlife_lifespan, wildlife_infectious_period,
+                           cattle_treatment_period, wildlife_lifespan, wildlife_infection_period,
                            wildlife_incubation_period, bite_rate, prob_infection_to_host, prob_infection_to_vector,
                            vector_teneral_period, days_between_feeds, prob_vector_surviving_feed, prob_vector_surviving_nonfeeding_day
     
@@ -47,6 +49,130 @@ calculate_vector_death_rate <- function(d, qf, qn, pi) {
   daily_mortality <- -log( (1 - pi) * qf * qn ^ d) / d
   daily_mortality
 }
+
+
+set_parameters_NEW <- function(this_scenario) {
+  birth_adj <- this_scenario$birth_adj
+  fit_adj <- this_scenario$fit_adj
+  K <- this_scenario$K
+  treat_prop <- this_scenario$treat_prop
+  prop_insecticide <- this_scenario$prop_insecticide
+  NW <- this_scenario$NW
+  prop_prophylaxis <- this_scenario$prop_prophylaxis
+  treatment_type <- this_scenario$treatment_type
+  dose_adj <- this_scenario$dose_adj
+  emergence_adj <- this_scenario$emergence_adj
+  
+  baseline_params <- set_baseline_parameters()
+  
+  ## Cattle -----
+  birth_c <- 1 / baseline_params["cattle_life_span"]
+  biterate <- 0.8 / 4
+  prob_infection_to_host <- 0.46
+  gamma_c <- 1 / 15
+  resusceptible <- 10
+  death_c <- birth_c
+  death_p <- death_c
+  sigma_c <- 1 / 100
+  treatment <- 1 * treat_prop * (sigma_c + death_c) / (1 - treat_prop)
+  emergence <- 0
+  
+  
+  if (treatment_type == "quick") {
+    treatment_q <- treatment
+    treatment_p <- 0
+    emergence_p <- 0
+    emergence_f <- emergence * emergence_adj
+  } else {
+    if (treatment_type == "P") {
+      treatment_q <- 0
+      treatment_p <- treatment
+      emergence_p <- emergence * emergence_adj
+      emergence_f <- 0
+    } else {
+      treatment_q <- treatment
+      treatment_p <- treatment
+      emergence_p <- emergence * emergence_adj
+      emergence_f <- emergence * emergence_adj
+    }
+  }
+  
+  sigma_st <- (1 / 3) * dose_adj + sigma_c * (1 - dose_adj) #* 250 #LM: adjusted so that R0 drops below 1 when 99% treated to reflect Hargrove
+  rec_adj <- 1
+  waning <- (1 / 30) / dose_adj
+  waning_f2s <- (1 / 60) / dose_adj
+  new_prop <- 0
+  
+  
+  NC <- 50 # Total cattle
+  # figure out equilibrium in absence of infection
+  # birth_c * prop_prophylaxis *NC - death_c * PF - waning_f2s*PF
+  PF <- birth_c * prop_prophylaxis * NC / (death_c + waning_f2s)
+  # waning_f2s * PF - death_p * PS - waning * PS
+  PS <- waning_f2s * PF / (death_p + waning)
+  # birth_c * (1-prop_prophylaxis) * NC - death_c * CS + waning * PS
+  CS <- (birth_c * (1 - prop_prophylaxis) * NC + waning * PS) / death_c
+  
+  ## ----- Wildlife
+  birth_w <- 1 / 365
+  prob_infection.s_w <- 0.46
+  prob_infection.r_w <- 0.46
+  gamma_w <- 1 / 20
+  resusceptible_w <- 1 / 100
+  death_w <- birth_w
+  sigma_w <- sigma_c
+  reversion <- 0
+  
+  ## -----  Vectors
+  
+  ten2fed <- 1 / 4
+  qf <- 0.96 # Probability of surviving on a feeding day
+  qn <- 0.98 # Probability of surviving on a non-feeding day
+  feed.cyc <- 4 # Days between feeding
+  feed.frequency <- 0
+  prob_infection_to_vector <- 0.025
+  incubation <- 20
+  prop_insecticide.actual <- prop_insecticide * NC / (NC + NW) # Proportion of insecticide adjusted for wildlife
+  death_v <- -1 * log((1 - prop_insecticide.actual) * qf * qn^feed.cyc) / feed.cyc # Vector death rate
+  birth_v <- birth_adj * (-1) * log((1 - 0) * qf * qn^feed.cyc) / feed.cyc # Vector birth rate
+  equil_vector_pop <- max(0, K * (1 - death_v / birth_v)) # Vector equilibrium population
+  gamma_v <- death_v * exp(-death_v * incubation) / (1 - exp(-death_v * incubation)) # Rate from E to I
+  
+  NV <- equil_vector_pop # equil_vector_pop
+  
+  ## ----- Parameters & initial conditions output
+  
+  params <- cbind(
+    NC, NV, NW, PF, PS, CS,
+    birth_c, biterate, prob_infection_to_host, fit_adj, rec_adj, sigma_st,
+    gamma_c, death_c, treatment_p, treatment_q, sigma_c, birth_v,
+    death_v, feed.frequency, prob_infection_to_vector, gamma_v, emergence_p, emergence_f,
+    reversion, K, birth_w, gamma_w, death_w, sigma_w, equil_vector_pop,
+    waning, waning_f2s, new_prop, ten2fed, prop_prophylaxis
+  )
+  names <- colnames(params)
+  params <- as.vector(params)
+  names(params) <- names
+  
+  qual_check_no0(params) # ensure there are no negative values
+  
+  return(params = params)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -177,19 +303,13 @@ create_params_for_output <- function(params) {
   fixed_params_for_output 
 }
 
+
 output_params <- function(params, report_time){
   
-  fixed_params_for_output <- create_params_for_output(params)
-  fixed_params_for_output <- signif(fixed_params_for_output, 4)
+  params_for_output <- set_baseline_parameters()
+  plot_this <- convert_named_vector_to_long_df(params_for_output)
   
-  for_plot1 <- fixed_params_for_output[c("cattle life span", "cattle inc period", "cattle inf period", "wildlife life span",
-                                         "wildlife inc period", "wildlife inf period", "vector lifespan",
-                                         "vector teneral period", "prob inf vector to host", "prob inf host to vector",
-                                         "cattle treatment period", "cattle proph full protection", "cattle proph partial protection")]
-  
-  df1 <- data.frame(value = as.numeric(for_plot1), name = names(for_plot1))
-  
-  p1 <- df1 %>% ggplot() +
+  p1 <- plot_this %>% ggplot() +
     geom_bar(aes(y = value, x = name), fill = "skyblue", stat = "identity") + ylab(" ") + xlab(" ") + ylim(c(0, 2000)) +
     geom_text(aes(label= signif(value, 4), y = value, x = name), vjust = -0.1, hjust = -0.2) +
     coord_flip() +
