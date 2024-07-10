6e42b2213f4008dd1fed59f86c991a2391c9621b
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-10 18:10:25 +0100

    renamed prop_insecticide

--- input_scenarios.R
+++ input_scenarios.R
@@ -1,6 +1,6 @@
 
 create_multiple_scenarios <- function() {
-  max_time <- 2
+  max_time <- 10000
   treatment_type <- "quick"
   cattle_number <- 50
   #wildlife_number <- c(0, 50, 100, 150, 200, 250)
@@ -9,9 +9,9 @@ create_multiple_scenarios <- function() {
   treat_propB <- seq(0.91, 0.99, by = 0.02)
   treat_prop <- c(treat_propA, treat_propB)
   carrying_capacity <- c(10000, 6000, 2000, 1000, 500)
-  # do not set prop_insecticide to 1 as generates infinite mortality and an error
-  prop_insecticide <- c(0.0, 0.05, 0.10, 0.15, 0.2, 0.5)
-  #prop_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
+  # do not set prop_cattle_with_insecticide to 1 as generates infinite mortality and an error
+  prop_cattle_with_insecticide <- c(0.0, 0.05, 0.10, 0.15, 0.2, 0.5)
+  #prop_cattle_with_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
   prop_prophylaxis <- 0.0
   fit_adj <- 0.95
   birth_adj <- 2.0
@@ -25,7 +25,7 @@ create_multiple_scenarios <- function() {
     NC = cattle_number, reversion = reversion, rec_adj = rec_adj, emergence = emergence, 
     emergence_adj = emergence_adj, dose_adj = dose_adj,
     treat_prop = treat_prop, NW = wildlife_number, K = carrying_capacity,
-    fit_adj = fit_adj, prop_insecticide = prop_insecticide,
+    fit_adj = fit_adj, prop_cattle_with_insecticide = prop_cattle_with_insecticide,
     birth_adj = birth_adj, prop_prophylaxis = prop_prophylaxis,
     treatment_type = treatment_type, max_time = max_time
   )
@@ -39,7 +39,7 @@ create_single_scenario <- function() {
   wildlife_number <- 250
   treat_prop <- 0.25
   carrying_capacity <- 2000
-  prop_insecticide <- 0.0
+  prop_cattle_with_insecticide <- 0.0
   prop_prophylaxis <- 0.0
   fit_adj <- 0.95
   birth_adj <- 2.0
@@ -53,7 +53,7 @@ create_single_scenario <- function() {
     NC = cattle_number, reversion = reversion, rec_adj = rec_adj, emergence = emergence, 
     emergence_adj = emergence_adj, dose_adj = dose_adj,
     treat_prop = treat_prop, NW = wildlife_number, K = carrying_capacity,
-    fit_adj = fit_adj, prop_insecticide = prop_insecticide,
+    fit_adj = fit_adj, prop_cattle_with_insecticide = prop_cattle_with_insecticide,
     birth_adj = birth_adj, prop_prophylaxis = prop_prophylaxis,
     treatment_type = treatment_type, max_time = max_time
   )
