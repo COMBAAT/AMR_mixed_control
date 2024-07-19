2a1677d29ca413cf99e19d1b456bb3022351b00a
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-19 17:09:17 +0100

    refactor functions to plot baseline parameters and scenarios

--- funcs/output_baseline_params_and_scenarios.R
+++ funcs/output_baseline_params_and_scenarios.R
@@ -1,58 +1,54 @@
 library(codetools)
 library(dplyr)
+library(ggplot2)
 library(patchwork)
+library(tidyr)
 
-output_baseline_params_as_dotplot <- function(){
+plot_baseline_parameters <- function(params){
   
-  params_for_output <- set_baseline_parameters()
-  plot_this <- convert_named_vector_to_long_df(params_for_output)
-  
-  ymax = 2200
+  plot_this <- convert_named_vector_to_long_df(params)
+  ymax = max(plot_this$value) * 1.2
   p <- plot_parameters(plot_this, ymax)
   p <- add_labels_to_baseline_parameters_dotplot(p, plot_this, ymax)
   p <- p + ggtitle("baseline parameters")
-  print(p)
-  
-  ggsave(p, filename = paste0("output/baseline_params_plot.pdf"))
-  write.csv(plot_this, paste0("output/baseline_params.csv"))
+  p
+}
+
+get_simplified_scenarios <- function(scenarios_df) {
+  scenarios_numeric <- scenarios_df %>% select(where(is.numeric))
+  scenarios_numeric_long <- pivot_longer(scenarios_numeric, everything(), names_to = "name", values_to = "value")
+  simplified_scenarios <- distinct(scenarios_numeric_long)
+  simplified_scenarios
 }
 
 
-output_scenarios_as_dotplot <- function(scenario_df) {
+plot_scenarios <- function(scenarios_df) {
   
-  scenario_numeric <- scenario_df %>% select(where(is.numeric))
-  scenario_numeric_long <- pivot_longer(scenario_numeric, everything(), names_to = "name", values_to = "value")
-  plot_this <- distinct(scenario_numeric_long)
+  simplified_scenarios <- get_simplified_scenarios(scenarios_df)
   
-  df <- plot_this %>% filter(name %in%  c("K"))
-  p1 <- plot_parameters(df, ymax = 10100)
-  p1 <- add_labels_to_scenarios_dotplot(p1, df)
+  plot_this <- simplified_scenarios %>% filter(name %in%  c("K"))
+  p1 <- plot_parameters(plot_this, ymax = 10100)
+  p1 <- add_labels_to_scenarios_dotplot(p1, plot_this)
   
-  df <- plot_this %>% filter(name %in%  c("birth_adj"))
-  p2 <- plot_parameters(df, ymax = 2.1)
-  p2 <- add_labels_to_scenarios_dotplot(p2, df)
+  plot_this <- simplified_scenarios %>% filter(name %in%  c("birth_adj"))
+  p2 <- plot_parameters(plot_this, ymax = 2.1)
+  p2 <- add_labels_to_scenarios_dotplot(p2, plot_this)
   
-  df <- plot_this %>% filter(name %in%  c("NC", "NW"))
-  p3 <- plot_parameters(df, ymax = 300)
-  p3 <- add_labels_to_scenarios_dotplot(p3, df)
+  plot_this <- simplified_scenarios %>% filter(name %in%  c("NC", "NW"))
+  p3 <- plot_parameters(plot_this, ymax = 300)
+  p3 <- add_labels_to_scenarios_dotplot(p3, plot_this)
   
-  df <- plot_this %>% filter(name %in% c("max_time"))
-  p4 <- plot_parameters(df, ymax = 10000)
-  p4 <- add_labels_to_scenarios_dotplot(p4, df)
+  plot_this <- simplified_scenarios %>% filter(name %in% c("max_time"))
+  p4 <- plot_parameters(plot_this, ymax = 10000)
+  p4 <- add_labels_to_scenarios_dotplot(p4, plot_this)
   
-  df <- plot_this %>% filter(name %in% c("option"))
-  p5 <- plot_parameters(df, ymax = 4.2)
-  p5 <- add_labels_to_scenarios_dotplot(p5, df)
+  plot_this <- simplified_scenarios %>% filter(!(name %in% c("NC", "NW", "K", "birth_adj", "max_time")))
+  p6 <- plot_parameters(plot_this, ymax = 1.0)
   
-  df <- plot_this %>% filter(!(name %in% c("NC", "NW", "K", "birth_adj", "max_time", "option")))
-  p6 <- plot_parameters(df, ymax = 1.0)
-  
-  p <- p1 + p2 + p3 + p4 + p5 + p6 + plot_layout(ncol = 1, heights = c(1, 1, 1, 1, 1, 3))
-  print(p)
-  
-  ggsave(p, filename = paste0("output/scenario_plot.pdf"))
-  write.csv(plot_this, paste0("output/scenario_params.csv"))
+  p <- p1 + p2 + p3 + p4 + p6 + plot_layout(ncol = 1, heights = c(1, 1, 1, 1, 3))
+  p
 }
+
 
 plot_parameters <- function(df, ymax) {
   my_size = 5
@@ -67,6 +63,7 @@ plot_parameters <- function(df, ymax) {
   p
 }
 
+
 add_labels_to_baseline_parameters_dotplot <- function(p, df, ymax){
   my_vjust <- 0.25
   my_hjust <- 0.5
@@ -74,6 +71,7 @@ add_labels_to_baseline_parameters_dotplot <- function(p, df, ymax){
                      vjust = my_vjust, hjust = my_hjust)
   p
 }
+
 
 add_labels_to_scenarios_dotplot <- function(p, df){
   my_vjust <- 0.5
@@ -84,8 +82,10 @@ add_labels_to_scenarios_dotplot <- function(p, df){
   p
 }
 
+
 findGlobals(fun = plot_parameters, merge = FALSE)$variables
 findGlobals(fun = add_labels_to_baseline_parameters_dotplot, merge = FALSE)$variables
 findGlobals(fun = add_labels_to_scenarios_dotplot, merge = FALSE)$variables
-findGlobals(fun = output_baseline_params_as_dotplot, merge = FALSE)$variables
-findGlobals(fun = output_scenarios_as_dotplot, merge = FALSE)$variables
+findGlobals(fun = plot_baseline_parameters, merge = FALSE)$variables
+findGlobals(fun = plot_scenarios, merge = FALSE)$variables
+
