3578f07bc3a7a46b17a6b27a7456b2b3e1f3a163
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-25 15:00:37 +0100

    adjusting inputs to explore scenarios

--- 1_set_user_inputs.R
+++ 1_set_user_inputs.R
@@ -7,19 +7,19 @@ set_days_per_year <- function() {
 
 get_user_inputs <- function() {
   user_inputs <- list(
-    multiple_scenarios = FALSE,
+    multiple_scenarios = TRUE,
     use_root_functions = FALSE,
     append_current_time_to_output_file = FALSE,
     folder = "output/",
     general_descriptor = "simulation_set_",
-    current_descriptor = "ORIGINAL_quick"
+    current_descriptor = "ORIGINAL_proph"
   )
   user_inputs
 }
 
 create_multiple_scenarios <- function() {
   max_time <- 10000
-  treatment_type <- "quick" # quick or proph
+  treatment_type <- "both" # quick or proph
   cattle_number <- 50
   #wildlife_number <- c(0, 50, 100, 150, 200, 250)
   wildlife_number <- c(0, 100, 250)
@@ -29,7 +29,7 @@ create_multiple_scenarios <- function() {
   carrying_capacity <- c(10000, 6000, 2000, 1000, 500)
   # do not set prop_cattle_with_insecticide to 1 as generates infinite mortality and an error
   prop_cattle_with_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.3, 0.4, 0.5)
-  prop_prophylaxis_at_birth <- 0.0 #seq(0, 0.9, 0.1)
+  prop_prophylaxis_at_birth <- 0.9 #seq(0, 0.9, 0.1)
   proph_ongoing <- 0.0 #seq(0, 0.9, 0.1)
   fit_adj <- 0.95
   birth_adj <- 2.0
