1bec455c3cbfc2b443c2f8e7accae44ac4b807ca
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-07-27 11:16:37 +0100

    added messages and issues function to user inputs

--- 1_set_user_inputs.R
+++ 1_set_user_inputs.R
@@ -1,5 +1,12 @@
 library(codetools)
 
+messages_and_issues <- function() {
+  issue1 <- "teneral stage not working as intended"
+  message1 <- "vector incubation has been wrongly calculated, enhance effect of insecticide"
+  message2 <- "added reversion for cattle stages"
+  message3 <- "need to include regular prophylactic treatment"
+}
+
 set_days_per_year <- function() {
   days_per_year <- 365.25
   days_per_year
@@ -12,14 +19,14 @@ get_user_inputs <- function() {
     append_current_time_to_output_file = FALSE,
     folder = "output/",
     general_descriptor = "simulation_set_",
-    current_descriptor = "ORIGINAL_quick"
+    current_descriptor = "NEW_quick"
   )
   user_inputs
 }
 
 create_multiple_scenarios <- function() {
   max_time <- 10000
-  treatment_type <- "quick" # quick or proph
+  treatment_type <- "quick" # quick, proph or both
   cattle_number <- 50
   #wildlife_number <- c(0, 50, 100, 150, 200, 250)
   wildlife_number <- c(0, 100, 250)
@@ -28,8 +35,8 @@ create_multiple_scenarios <- function() {
   treat_prop <- c(treat_propA, treat_propB)
   carrying_capacity <- c(10000, 6000, 2000, 1000, 500)
   # do not set prop_cattle_with_insecticide to 1 as generates infinite mortality and an error
-  prop_cattle_with_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.15, 0.2, 0.3, 0.4, 0.5)
-  prop_prophylaxis_at_birth <- 0.9 #seq(0, 0.9, 0.1)
+  prop_cattle_with_insecticide <- c(0.0, 0.025, 0.05, 0.10, 0.0125, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5)
+  prop_prophylaxis_at_birth <- 0.0 #seq(0, 0.9, 0.1)
   proph_ongoing <- 0.0 #seq(0, 0.9, 0.1)
   fit_adj <- 0.95
   birth_adj <- 2.0
@@ -51,13 +58,13 @@ create_multiple_scenarios <- function() {
 
 create_single_scenario <- function() {
   max_time <- 5000
-  treatment_type <- "proph"
+  treatment_type <- "both" # quick, proph or both
   cattle_number <- 50
   wildlife_number <- 250
-  treat_prop <- 0.1
+  treat_prop <- 0.5
   carrying_capacity <- 2000
   prop_cattle_with_insecticide <- 0.0
-  prop_prophylaxis_at_birth <- 0.0
+  prop_prophylaxis_at_birth <- 0.9
   proph_ongoing <- 0.0 #seq(0, 0.9, 0.1)
   fit_adj <- 0.95
   birth_adj <- 2.0
