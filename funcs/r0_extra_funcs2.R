723fa952706c82684605a3ac0b968a2fb7ff90b7
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-11-18 09:16:09 +0000

    calculate loop probabilites functionadded to r0_extra_funcs2.R

--- funcs/r0_extra_funcs2.R
+++ funcs/r0_extra_funcs2.R
@@ -1,3 +1,22 @@
+calculate_loop_probabilities <- function(params, is_strain_sensitive) {
+  loop_probabiities <- with(as.list(params, is_strain_sensitive), {
+    
+    if (is_strain_sensitive == "yes") {
+      sigma_treated <- sigma_st
+    }
+    if (is_strain_sensitive == "no") {
+      sigma_treated <- sigma_c
+    }
+    
+    # sum of probabilities of transitioning from PI to PP, from PI to PT, and from PI to CI
+    p1c <- prob_PI_treat_p + prob_proph_from_PI + prob_waning_from_partial_protection_from_PI * (prob_CI_treat_p + prob_proph_from_CI)
+    # sum of probabilities of transitioning from PP to PI
+    p2c <- prob_waning_from_partial_protection_from_PP
+    list(p1c = p1c, p2c = p2c)
+  })
+  loop_probabiities
+}
+
 
 calculate_time2A <- function(prob_CI_treat_q, prob_CI_treat_p, prob_proph_from_CI, time_in_CI, time_in_CT, time_in_PP) {
   time2A <- (time_in_CI +
@@ -25,12 +44,12 @@ calculate_time2B <- function(prob_PT_treat_p, prob_waning_from_partial_protectio
     prob_PI_treat_q * time_in_PT +
     prob_PI_treat_q * prob_waning_from_partial_protection_from_PT * time_in_CT +
     prob_PI_treat_p * time_in_PP +
-    prob_proph_from_PI * time_in_PP) / (1 - p1 * p2)
+    prob_proph_from_PI * time_in_PP) / (1 - p1c * p2c)
   time2B
 }
 
 #################################################################################
-calculate_RVC2 <- function(time_in_CI, prob_CI_treat_q, prob_CI_treat_p, prob_proph_from_CI, prob_proph_from_PE, prob_disease_from_CEX,
+calculate_RVC <- function(time_in_CI, prob_CI_treat_q, prob_CI_treat_p, prob_proph_from_CI, prob_proph_from_PE, prob_disease_from_CEX,
                            rate_vectors_infected, p1c, p2c, prob_waning_from_partial_protection_from_PP, time_in_PP, prob_proph_from_CE,
                            prob_CI_from_CE, time_in_CT) {
   time2A <- calculate_time2A(prob_CI_treat_q, prob_CI_treat_p, prob_proph_from_CI, time_in_CI, time_in_CT, time_in_PP)
@@ -48,7 +67,7 @@ calculate_RVC2 <- function(time_in_CI, prob_CI_treat_q, prob_CI_treat_p, prob_pr
   RVC
 }
 
-calculate_RPV2 <- function(prob_PI_from_PE, prob_waning_from_partial_protection_from_PE, prob_proph_from_PE,
+calculate_RPV <- function(prob_PI_from_PE, prob_waning_from_partial_protection_from_PE, prob_proph_from_PE,
                            prob_disease_from_PEX, prob_PP_from_PE,
                            prob_waning_from_partial_protection_from_PP,
                            prob_PT_treat_p, prob_waning_from_partial_protection_from_PT,
@@ -78,8 +97,5 @@ calculate_RPV2 <- function(prob_PI_from_PE, prob_waning_from_partial_protection_
 
 findGlobals(fun = calculate_time2A, merge = FALSE)$variables
 findGlobals(fun = calculate_time2B, merge = FALSE)$variables
-findGlobals(fun = calculate_RVC2, merge = FALSE)$variables
-findGlobals(fun = calculate_RPV2, merge = FALSE)$variables
 findGlobals(fun = calculate_RVC, merge = FALSE)$variables
-findGlobals(fun = calculate_RVP, merge = FALSE)$variables
-findGlobals(fun = calculate_R1, merge = FALSE)$variables
+findGlobals(fun = calculate_RPV, merge = FALSE)$variables
