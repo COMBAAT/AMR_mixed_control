15c7a748535febb55013b92d8991b05b48ae1ee1
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2024-11-19 20:44:32 +0000

    moved version2 functions to funcs2

--- funcs2/r0_extra_funcs2.R
+++ funcs2/r0_extra_funcs2.R
@@ -1,30 +1,34 @@
 calculate_loop_probabilities <- function(transition_probabilities, is_strain_sensitive) {
   loop_probabiities <- with(as.list(transition_probabilities, is_strain_sensitive), {
     
-    pCItoPI <- (prob_CI_treat_p + prob_proph_from_CI) * ( 1 + prob_waning_from_partial_protection_from_PP)
+    pCItoPI <- (prob_CI_treat_p + prob_proph_from_CI) * prob_waning_from_partial_protection_from_PP
     pPItoCI <- prob_waning_from_partial_protection_from_PI
     
-    pPItoPP <- prob_PI_treat_p + prob_proph_from_PI + prob_waning_from_partial_protection_from_PI * (prob_CI_treat_p + prob_proph_from_CI)
+    pPItoPP <- prob_PI_treat_p + prob_proph_from_PI
     pPPtoPI <- prob_waning_from_partial_protection_from_PP
     
+    ploop1toloop2 <- pCItoPI * pPItoPP * pPPtoPI
+    ploop2toloop1 <- pPItoCI 
+    
     # sum of probabilities of transitioning from PI to PP
-    p1c <- prob_PI_treat_p + prob_proph_from_PI + prob_waning_from_partial_protection_from_PI * (prob_CI_treat_p + prob_proph_from_CI)
+    #p1c <- prob_PI_treat_p + prob_proph_from_PI + prob_waning_from_partial_protection_from_PI * (prob_CI_treat_p + prob_proph_from_CI)
     # sum of probabilities of transitioning from PP to PI
-    p2c <- prob_waning_from_partial_protection_from_PP
-    list(p1c = p1c, p2c = p2c, pCItoPI = pCItoPI, pPItoCI = pPItoCI, pPItoPP = pPItoPP, pPPtoPI = pPPtoPI)
+    #p2c <- prob_waning_from_partial_protection_from_PP
+    list(pCItoPI = pCItoPI, pPItoCI = pPItoCI, pPItoPP = pPItoPP, pPPtoPI = pPPtoPI, 
+         ploop1toloop2 = ploop1toloop2, ploop2toloop1 = ploop2toloop1)
   })
   loop_probabiities
 }
 
 
 calculate_time2A <- function(time_in_state, transition_probabilities) {
-  time2B <- calculate_time2B(time_in_state, transition_probabilities)
-  time2A <- with(as.list(c(time_in_state, transition_probabilities, time2B)), {
+  #time2B <- calculate_time2B(time_in_state, transition_probabilities)
+  time2A <- with(as.list(c(time_in_state, transition_probabilities)), {
     time2A <- (time_in_CI +
       prob_CI_treat_q * time_in_CT +
       prob_CI_treat_p * time_in_PP +
       prob_proph_from_CI * time_in_PP +
-      pCItoPI * time2B) / (1 - pCItoPI * pPItoCI)
+      pCItoPI * time_in_PI) / (1 - pCItoPI * pPItoCI)
     time2A
   })
   time2A
@@ -54,11 +58,12 @@ calculate_RVC <- function(rate_vectors_infected, time_in_state, transition_proba
     
     #prob_CI_from_CE <- prob_CI_treat_p + prob_proph_from_CI #?
     #print(prob_CI_from_CE)
-    component1 <- prob_CI_from_CE * time2A
+    component1 <- prob_CI_from_CE * (time2A + ploop1toloop2 * time2B)/(1 - ploop1toloop2 * ploop2toloop1)
     
     prob_PP_from_CE <- prob_proph_from_CE * prob_disease_from_CEX
-    component2 <- prob_PP_from_CE * time_in_PP + prob_PP_from_CE * prob_waning_from_partial_protection_from_PP * time2B +
-      prob_PP_from_CE * prob_waning_from_partial_protection_from_PP * prob_waning_from_partial_protection_from_PI * time2A
+    component2 <- prob_PP_from_CE * time_in_PP + 
+      prob_PP_from_CE * prob_waning_from_partial_protection_from_PP * (time2B + ploop2toloop1 * time2A)/(1 - ploop1toloop2 * ploop2toloop1)
+      #prob_PP_from_CE * prob_waning_from_partial_protection_from_PP * prob_waning_from_partial_protection_from_PI * time2A
     
     # transmission via C
     RVC <- (component1 + component2) * rate_vectors_infected
@@ -75,12 +80,13 @@ calculate_RVP <- function(rate_vectors_infected, time_in_state, transition_proba
 
   RVP <- with(as.list(c(time_in_state, transition_probabilities, rate_vectors_infected, time2B, RVC)), {
   
-  component1 <- prob_PI_from_PE * time2B + prob_PI_from_PE * prob_waning_from_partial_protection_from_PI * time2A
-  component2 <- prob_waning_from_partial_protection_from_PE * RVC
+  component1 <- prob_PI_from_PE * (time2B + ploop2toloop1 * time2A)/(1 - ploop1toloop2 * ploop2toloop1)
 
   prob_PP_from_PE <- prob_proph_from_PE * prob_disease_from_PEX
-  component3 <- prob_PP_from_PE * time_in_PP + prob_PP_from_PE * prob_waning_from_partial_protection_from_PP * time2B +
-    prob_PP_from_PE * prob_waning_from_partial_protection_from_PP * prob_waning_from_partial_protection_from_PI * time2A
+  component2 <- prob_PP_from_PE * time_in_PP + 
+    prob_PP_from_PE * prob_waning_from_partial_protection_from_PP * (time2B + ploop2toloop1 * time2A)/(1 - ploop1toloop2 * ploop2toloop1)
+  
+  component3 <- prob_waning_from_partial_protection_from_PE * RVC
 
   RVP <- (component1 + component2 + component3) * rate_vectors_infected
   RVP <- as.numeric(RVP)
