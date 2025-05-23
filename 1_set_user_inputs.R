a12466d093dad1172ac9899f9ed148d650ecc758
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-05-23 22:35:29 +0100

    minor scenario changes

--- 1_set_user_inputs.R
+++ 1_set_user_inputs.R
@@ -11,7 +11,7 @@ get_user_inputs <- function() {
     use_root_functions = TRUE,
     append_current_time_to_output_file = FALSE,
     folder = "output/",
-    general_descriptor = "April19",
+    general_descriptor = "May24",
     current_descriptor = "quick_proph"
   )
   user_inputs
