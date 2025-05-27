4d33eee1c3a997fe2f49fa5e5949b2209b980e69
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-05-27 10:55:08 +0100

    changed bite_frac_cattle and bite_frac_wildlife back to reflect bites proportional to abundance among hosts

--- funcs/helper_functions.R
+++ funcs/helper_functions.R
@@ -491,14 +491,14 @@ bite_frac_cattle  <- function(NC, N) {
   # Function to calculate the fraction of bites on cattle
   # It calculates the fraction of bites on cattle based on the number of cattle and wildlife
   NC / N
-  0.6
+  #0.6
 }
 
 bite_frac_wildlife <- function(NW, N) {
   # Function to calculate the fraction of bites on wildlife
   # It calculates the fraction of bites on wildlife based on the number of wildlife and total population
   NW / N
-  0.4
+  #0.4
 }
 
 
