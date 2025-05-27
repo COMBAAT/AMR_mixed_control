a0f8d434eac7c1d8282e9cf1edc3ea5373c5b0fe
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-05-27 21:34:40 +0100

    added new create_single_scenario() function to replicate format of create_multiple_scenarios()

--- 1_set_user_inputs.R
+++ 1_set_user_inputs.R
@@ -69,7 +69,65 @@ create_multiple_scenarios <- function() {
   df
 }
 
+
 create_single_scenario <- function() {
+  days_per_year <- set_days_per_year()
+  max_time <- 5000
+  treatment_type <- c("curative") # curative or longlasting
+  cattle_number <- 100
+  wildlife_number <- 100
+  use_carrying_capacity <- TRUE # whether to use carrying capacity or host_vector_ratio
+  carrying_capacity <- 6000
+  host_vector_ratio <- 30
+  treat_prop <- 0.1 + 0.1*runif(1) 
+  # do not set prop_cattle_with_insecticide to 1 as generates infinite mortality and an error
+  prop_cattle_with_insecticide <- 0.05
+  maintain_vector_pop <- TRUE # whether to maintain vector population at carrying capacity or not)
+  prop_prophylaxis_at_birth <- 0.0
+  proph_ongoing <- 0 / days_per_year # c(0, 2, 4) / days_per_year
+  fit_adj <- 0.8
+  birth_adj <- 2.0
+  dose_adj <- 1.0
+  emergence <- 0.0
+  partial_susceptibility_proph_cattle <- 0.5
+  
+  # create grids of parameters combinations and then combine
+  tb1 <- expand_grid(
+    emergence = emergence,
+    dose_adj = dose_adj, proph_ongoing = proph_ongoing,
+    partial_susceptibility_proph_cattle = partial_susceptibility_proph_cattle,
+    treat_prop = treat_prop, maintain_vector_pop = maintain_vector_pop,
+    fit_adj = fit_adj, prop_cattle_with_insecticide = prop_cattle_with_insecticide,
+    birth_adj = birth_adj, prop_prophylaxis_at_birth = prop_prophylaxis_at_birth,
+    treatment_type = treatment_type, max_time = max_time
+  )
+  
+  tb_hosts <- expand_grid(NC = cattle_number, NW = wildlife_number) %>% mutate(hosts = NC + NW)
+  
+  tb2a <- expand_grid(tb_hosts, K = carrying_capacity) %>%
+    mutate(use_carrying_capacity = TRUE, host_vector_ratio = K / hosts) %>% 
+    select(NC, NW, hosts, use_carrying_capacity, host_vector_ratio, K)
+  
+  tb2b <- expand_grid(tb_hosts, host_vector_ratio = host_vector_ratio) %>%
+    mutate(use_carrying_capacity = FALSE, K = hosts * host_vector_ratio) %>% 
+    select(NC, NW, hosts, use_carrying_capacity, host_vector_ratio, K)
+  
+  if (use_carrying_capacity == TRUE) {
+    tb2 <- tb2a 
+  } else {
+    tb2 <- tb2b
+  }
+  
+  tb <- expand_grid(tb2, tb1)
+  
+  df <- as.data.frame(tb) %>% mutate(treatment_type = as.factor(treatment_type))
+  df
+}
+
+
+
+
+create_single_scenario_OLD <- function() {
   days_per_year <- set_days_per_year()
   max_time <- 5000
   treatment_type <- "curative" # quick or proph
