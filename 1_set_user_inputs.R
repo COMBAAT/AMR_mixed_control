4a3879a8bede98cb3451bfc99da739a9e06ed845
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-06-03 22:07:50 +0100

    updates to scenarios

--- 1_set_user_inputs.R
+++ 1_set_user_inputs.R
@@ -8,10 +8,10 @@ set_days_per_year <- function() {
 get_user_inputs <- function() {
   user_inputs <- list(
     multiple_scenarios = TRUE,
-    use_root_functions = TRUE,
+    use_root_functions = FALSE,
     append_current_time_to_output_file = FALSE,
     folder = "output/",
-    general_descriptor = "June3_short",
+    general_descriptor = "play",
     current_descriptor = "curative_longlasting"
   )
   user_inputs
@@ -19,7 +19,7 @@ get_user_inputs <- function() {
 
 create_multiple_scenarios <- function() {
   days_per_year <- set_days_per_year()
-  max_time <- 10
+  max_time <- 100
   treatment_type <- c("curative", "longlasting") # curative or longlasting
   cattle_number <- 100
   wildlife_number <- c(0, 100, 300)
@@ -27,9 +27,9 @@ create_multiple_scenarios <- function() {
   K_host_ratio <- seq(10, 50, by = 20)      # carrying capacity per host
   treat_propA <- seq(0.0, 0.9, by = 0.1)
   treat_propB <- seq(0.91, 0.99, by = 0.04)
-  treat_prop <- c(treat_propA, treat_propB) # treatment proportion of cattle with trypanocides
+  treat_prop <- c(0.0, 0.1) #c(treat_propA, treat_propB) # treatment proportion of cattle with trypanocides
   # do not set prop_cattle_with_insecticide to 1
-  prop_cattle_with_insecticide <- seq(0.0, 0.5, by = 0.05)
+  prop_cattle_with_insecticide <- seq(0.0, 0.5, by = 0.25)
   maintain_vector_pop <- c(TRUE, FALSE) # whether to maintain vector population at carrying capacity or not
   prop_prophylaxis_at_birth <- c(0.0)
   proph_ongoing <- 0 / days_per_year # c(0, 2, 4) / days_per_year
@@ -75,13 +75,13 @@ create_multiple_scenarios <- function() {
 create_single_scenario <- function() {
   days_per_year <- set_days_per_year()
   max_time <- 3000
-  treatment_type <- c("curative") # curative or longlasting
+  treatment_type <- c("longlasting") # curative or longlasting
   cattle_number <- 100
   wildlife_number <- 100
   use_carrying_capacity <- TRUE # whether to use carrying capacity or K_host_ratio
   carrying_capacity <- 6000 # carrying capacity of vector population
   K_host_ratio <- 30 # carrying capacity per host
-  treat_prop <- 0.1 + 0.1*runif(1) 
+  treat_prop <- 0.6
   # do not set prop_cattle_with_insecticide to 1
   prop_cattle_with_insecticide <- 0.0
   maintain_vector_pop <- FALSE # whether to maintain vector population at carrying capacity or not)
