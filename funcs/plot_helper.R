7372d68f24c1c5f82e2d0d03a8fdfcc86225ca04
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-07-22 11:46:09 +0100

    added plotye0_ratio() to plot_helper.R and deleted plot_helper_extra.R

--- funcs/plot_helper.R
+++ funcs/plot_helper.R
@@ -120,7 +120,32 @@ my_theme <- function() {
 }
 
 # Specify plot functions -------------------------------------------------------
-
+plot_type0_ratio <- function(df, this_vector_measure, ttype) {
+  x_var <- get_treat_var(df, ttype)
+  df$x <- df[, x_var]
+  this_xlab <- my_label(x_var)
+  
+  df$shape_variable <- df[, this_vector_measure]
+  lhs <- df %>%
+    mutate_at(c("prop_cattle_with_insecticide", "NW", this_vector_measure, "shape_variable"), as.factor) %>%
+    filter(prop_cattle_with_insecticide == 0.0) %>%
+    ggplot(aes(treat_prop, ratio, colour = NW, shape = shape_variable)) +
+    geom_point(size = my_pointsize()) +
+    geom_line(linewidth = my_linewidth()) +
+    xlab(this_xlab) +
+    ylab(my_label("ratio")) +
+    labs(colour = my_label("NW"), shape = my_label(this_vector_measure)) +
+    my_theme()
+  
+  rhs <- lhs + ylim(c(0, 2)) +
+    geom_abline(intercept = 1.0, slope = 0, linetype = "dashed")
+  rhs
+  
+  # use patchwork package to stick plots together
+  # use guides = collect to remove duplicate legends
+  p <- lhs + rhs + plot_layout(ncol = 2, guides = "collect", axis_titles = "collect")
+  p
+}
 
 #-------------------------------------------------------------------------------
 # Function Name: plot_type1_y_versus_treat_prop_facet_NW
