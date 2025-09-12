d25c5f8ceaaf6d3ee692aeba45fe1e9e32efd1f7
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2025-09-12 14:13:48 +0100

    renamed figs and deleted vertical figs

--- 5_generate_patchwork_plots_for_ms.R
+++ 5_generate_patchwork_plots_for_ms.R
@@ -1,5 +1,5 @@
 # Generating plots for the manuscript
-rm(list = ls()[!grepl("^(plot|df|this_NW|this_vector_measure)", ls())])
+rm(list = ls()[!grepl("^(plot|df|this_NW_set|this_vector_measure)", ls())])
 source("funcs/plot_helper.R")
 library(patchwork)
 
@@ -10,16 +10,16 @@ names(plots2F)[grepl("plot_type1", names(plots2F))]
 
 # ##############################################################################
 # Figure S1
-figX.1a <- plots1F$plot_type1_R0sen_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE
-figX.2a <- plots1F$plot_type1_prevalence_ttype1_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE
-figX.3a <- plots1F$plot_type1_Incidence_ttype1_spec_FALSE_FALSE
-figX.3b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE
-figX.4a <- plots1F$plot_type1_No_trt_cat_ttype1_spec_FALSE_FALSE
-figX.4b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE
-figX.5a <- plots1F$plot_type1_RiskA_ttype1_spec_FALSE_FALSE
-figX.5b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE
+figX.1a <- plots1F$plot_type1_R0sen_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots1F$plot_type1_prevalence_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.3a <- plots1F$plot_type1_Incidence_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.3b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.4a <- plots1F$plot_type1_No_trt_cat_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.4b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.5a <- plots1F$plot_type1_RiskA_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.5b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Curative drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Long-lasting drug\n ")) +
@@ -51,16 +51,16 @@ print("here1")
 
 # ##############################################################################
 # Figure S2
-figX.1a <- plots3F$plot_type1_R0sen_ttype3_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE
-figX.2a <- plots3F$plot_type1_prevalence_ttype3_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE
-figX.3a <- plots3F$plot_type1_Incidence_ttype3_spec_FALSE_FALSE
-figX.3b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE
-figX.4a <- plots3F$plot_type1_No_trt_cat_ttype3_spec_FALSE_FALSE
-figX.4b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE
-figX.5a <- plots3F$plot_type1_RiskA_ttype3_spec_FALSE_FALSE
-figX.5b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE
+figX.1a <- plots3F$plot_type1_R0sen_ttype3_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots3F$plot_type1_prevalence_ttype3_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.3a <- plots3F$plot_type1_Incidence_ttype3_spec_FALSE_FALSE_vector_value_20
+figX.3b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.4a <- plots3F$plot_type1_No_trt_cat_ttype3_spec_FALSE_FALSE_vector_value_20
+figX.4b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.5a <- plots3F$plot_type1_RiskA_ttype3_spec_FALSE_FALSE_vector_value_20
+figX.5b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Ongoing long-lasting drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Responsive long-lasting drug\n ")) +
@@ -88,7 +88,7 @@ figX <- figX.1 / figX.2 / figX.3 / figX.4 / figX.5 + plot_layout(guides = 'colle
   #theme_bw(base_size = 16) & 
   theme(legend.position = "bottom") 
 figX
-#ggsave("output/ms_figs/pwS2.pdf", figX, width = 11, height = 13) # best size
+ggsave("output/ms_figs/pwS2.pdf", figX, width = 11, height = 13) # best size
 
 # ##############################################################################
 
@@ -98,10 +98,10 @@ figX
 # mostly captures the shortening of the infected period
 # rather than a reduction in force of infection
 # as demonstrated by the incidence patterns
-figX.1a <- plots1F$plot_type1_prevalence_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE
-figX.2a <- plots1F$plot_type1_Incidence_ttype1_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE
+figX.1a <- plots1F$plot_type1_prevalence_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type1_prevalence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots1F$plot_type1_Incidence_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type1_Incidence_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Curative drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Long-lasting drug\n ")) +
@@ -125,10 +125,10 @@ ggsave("output/ms_figs/pw2.pdf", figX, width = 11, height = 6.5) # best size
 # mostly captures the shortening of the infected period
 # rather than a reduction in force of infection
 # as demonstrated by the incidence patterns
-figX.1a <- plots1F$plot_type1_RiskA_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE
-figX.2a <- plots1F$plot_type1_RiskE_ttype1_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type1_RiskE_ttype2_spec_FALSE_FALSE
+figX.1a <- plots1F$plot_type1_RiskA_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type1_RiskA_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots1F$plot_type1_RiskE_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type1_RiskE_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Curative drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Long-lasting drug\n ")) +
@@ -147,10 +147,10 @@ figX
 ggsave("output/ms_figs/pw2_options.pdf", figX, width = 11, height = 6.5) # best size
 # ##############################################################################
 # Figure S1
-figX.1a <- plots1F$plot_type1_R0sen_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE
-figX.2a <- plots1F$plot_type1_No_trt_cat_ttype1_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE
+figX.1a <- plots1F$plot_type1_R0sen_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type1_R0sen_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots1F$plot_type1_No_trt_cat_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type1_No_trt_cat_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Curative drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Long-lasting drug\n ")) +
@@ -173,14 +173,14 @@ ggsave("output/ms_figs/pw1_small.pdf", figX, width = 11, height = 6.5) # best si
 
 # ##############################################################################
 # Figure 3 shows the impact of insecticide resistance
-figX.1a <- plots1F$plot_type4_R0sen_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type4_R0sen_ttype2_spec_FALSE_FALSE
-figX.1a <- plots1F$plot_type4_No_trt_cat_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type4_No_trt_cat_ttype2_spec_FALSE_FALSE
-figX.1a <- plots1F$plot_type4_prevalence_ttype1_spec_FALSE_FALSE
-figX.1b <- plots2F$plot_type4_prevalence_ttype2_spec_FALSE_FALSE
-figX.2a <- plots1F$plot_type4_RiskA_ttype1_spec_FALSE_FALSE
-figX.2b <- plots2F$plot_type4_RiskA_ttype2_spec_FALSE_FALSE
+figX.1a <- plots1F$plot_type4_R0sen_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type4_R0sen_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.1a <- plots1F$plot_type4_No_trt_cat_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type4_No_trt_cat_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.1a <- plots1F$plot_type4_prevalence_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.1b <- plots2F$plot_type4_prevalence_ttype2_spec_FALSE_FALSE_vector_value_20
+figX.2a <- plots1F$plot_type4_RiskA_ttype1_spec_FALSE_FALSE_vector_value_20
+figX.2b <- plots2F$plot_type4_RiskA_ttype2_spec_FALSE_FALSE_vector_value_20
 
 figX.1 <- (figX.1a + labs(title = "A") + ggtitle("Curative drug\n ") + plot_spacer() +
              figX.1b + labs(title = "B") + ggtitle("Long-lasting drug\n ")) +
@@ -199,33 +199,15 @@ figX
 ggsave("output/ms_figs/pw3.pdf", figX, width = 11, height = 6.5) # best size
 
 # ##############################################################################
-# Figure 4 shows the impact of insecticide resistance
-figX.1 <- plots1F$plot_type4vert_RiskA_ttype1_spec_FALSE_FALSE + ggtitle(my_title("curative")) 
-figX.2 <- plots2F$plot_type4vert_RiskA_ttype2_spec_FALSE_FALSE + ggtitle(my_title("longlasting")) +
-  labs(y = NULL) +  # remove axis label
-  theme(
-    axis.text.y = element_blank(),     # remove tick labels
-    axis.ticks.y = element_blank()     # remove tick marks
-  )
-figX.2
-figX.3 <- plots3F$plot_type4vert_RiskA_ttype3_spec_FALSE_FALSE + ggtitle(my_title("ongoing")) +
-  labs(y = NULL) +  # remove axis label
-  theme(
-    axis.text.y = element_blank(),     # remove tick labels
-    axis.ticks.y = element_blank()     # remove tick marks
-  )
-
-figX <- (figX.1 | figX.2 | plot_spacer() | figX.3) + plot_layout(guides = 'collect', axes = "collect", widths = c(1, 1, 0.1, 1)) & 
-  #plot_annotation(tag_levels = 'A') & 
-  #theme_bw(base_size = 16) & 
-  theme(legend.position = "bottom") 
-ggsave("output/ms_figs/pw4.pdf", figX, width = 8.5, height = 8.5) # best size
 
 # ##############################################################################
 # Figure 3 shows the impact of insecticide resistance
-figX.1 <- plots1F$plot_type5_RiskA_ttype1_spec_FALSE_FALSE + ggtitle(my_title("curative", split_across_lines = "other")) + guides(shape = "none")
-figX.2 <- plots2F$plot_type5_RiskA_ttype2_spec_FALSE_FALSE + ggtitle(my_title("longlasting", split_across_lines = "other")) + guides(shape = "none")
-figX.3 <- plots3F$plot_type5_RiskA_ttype3_spec_FALSE_FALSE + ggtitle(my_title("ongoing", split_across_lines = "other")) + guides(shape = "none")
+figX.1 <- plots1F$plot_type5_RiskA_ttype1_spec_FALSE_FALSE_vector_value_20 + 
+  ggtitle(my_title("curative", split_across_lines = "other")) + guides(shape = "none")
+figX.2 <- plots2F$plot_type5_RiskA_ttype2_spec_FALSE_FALSE_vector_value_20 + 
+  ggtitle(my_title("longlasting", split_across_lines = "other")) + guides(shape = "none")
+figX.3 <- plots3F$plot_type5_RiskA_ttype3_spec_FALSE_FALSE_vector_value_20 + 
+  ggtitle(my_title("ongoing", split_across_lines = "other")) + guides(shape = "none")
 
 
 figA <- ( (figX.1) / figX.2 ) + plot_layout(guides = 'collect') & 
@@ -242,15 +224,15 @@ ggsave("output/ms_figs/use_as_fig4.pdf", fig4, width = 8, height = 10) # best si
 # ##############################################################################
 # Figure 5 
 
-figX.1 <- plots1F$plot_type4_Rres_final_ttype1_spec_FALSE_FALSE + 
+figX.1 <- plots1F$plot_type4_Rres_final_ttype1_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("curative", split_across_lines = "other")) + #guides(shape = "none") + 
   coord_cartesian(ylim = c(0, 5)) +
   geom_segment(x = 0.0, y = 1.0, xend = 1.0, yend = 1.0, colour = "red", linewidth = 0.5)
-figX.2 <- plots2F$plot_type4_Rres_final_ttype2_spec_FALSE_FALSE + 
+figX.2 <- plots2F$plot_type4_Rres_final_ttype2_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("longlasting", split_across_lines = "other")) + #guides(shape = "none") + 
   coord_cartesian(ylim = c(0, 5)) +
   geom_segment(x = 0.0, y = 1.0, xend = 1.0, yend = 1.0, colour = "red", linewidth = 0.5)
-figX.3 <- plots3F$plot_type4_Rres_final_ttype3_spec_FALSE_FALSE + 
+figX.3 <- plots3F$plot_type4_Rres_final_ttype3_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("ongoing", split_across_lines = "other")) + #guides(shape = "none") + 
   coord_cartesian(ylim = c(0, 5)) +
   geom_segment(x = 0.0, y = 1.0, xend = 12.0, yend = 1.0, colour = "red", linewidth = 0.5)
@@ -269,15 +251,15 @@ ggsave("output/ms_figs/use_as_fig5B.pdf", figB, width = 8, height = 3.5) # best 
 # ##############################################################################
 # Figure 5 
 
-figX.1 <- plots1F$plot_type4v2_Rres_final_ttype1_spec_FALSE_FALSE + 
+figX.1 <- plots1F$plot_type6_Rres_final_ttype1_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("curative", split_across_lines = "other")) + #guides(shape = "none") + 
 coord_cartesian(ylim = c(0, 10)) #+
   #geom_segment(x = 0.0, y = 1.0, xend = 1.0, yend = 1.0, colour = "red", linewidth = 0.5)
-figX.2 <- plots2F$plot_type4v2_Rres_final_ttype2_spec_FALSE_FALSE + 
+figX.2 <- plots2F$plot_type6_Rres_final_ttype2_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("longlasting", split_across_lines = "other")) + #guides(shape = "none") + 
 coord_cartesian(ylim = c(0, 10)) #+
   #geom_segment(x = 0.0, y = 1.0, xend = 1.0, yend = 1.0, colour = "red", linewidth = 0.5)
-figX.3 <- plots3F$plot_type4v2_Rres_final_ttype3_spec_FALSE_FALSE + 
+figX.3 <- plots3F$plot_type6_Rres_final_ttype3_spec_FALSE_FALSE_vector_value_20 + 
   ggtitle(my_title("ongoing", split_across_lines = "other")) + #guides(shape = "none") + 
 coord_cartesian(ylim = c(0, 10)) #+
   #geom_segment(x = 0.0, y = 1.0, xend = 12.0, yend = 1.0, colour = "red", linewidth = 0.5)
