5571d126231b07467764b8260c360d663597f848
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2026-01-30 22:05:51 +0000

    Added Deaths due to disease to epi outputs

--- funcs/epi_outputs.R
+++ funcs/epi_outputs.R
@@ -63,7 +63,9 @@ append_epi_outputs_to_df <- function(df) {
     Incidence_P = gamma_c * (PEs_final + PEsX_final) * days_per_year,
     prevalence_wildlife = WIs_final / Wildlife_total_final, 
     prevalence_vectors = VIs_final / Vector_total_final,
-    waning_from_PI =  waning_from_PI * PIs_final * days_per_year
+    waning_from_PI =  waning_from_PI * PIs_final * days_per_year,
+    Deaths_due_disease = death_dis * (CIs_final + PIs_final) * days_per_year,
+    Deaths_due_disease2 = prob_death_from_disease * (1 - treat_prop) * Incidence_new
   )
   df
 }
