ab6cbd569a3907be3561200ed2ffd0d19abbaa1d
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2026-02-17 21:11:05 +0000

    moved epi plotting functions into folder epi_plotting_functions

--- epi_plotting_functions/8_generate_epi_plots.R
+++ epi_plotting_functions/8_generate_epi_plots.R
@@ -1,17 +1,17 @@
 library(tictoc)
 # generate initial plots
 tic()
-source("3_plot_explore_test.R")
+source("epi_plotting_functions/3_plot_explore_test.R")
 toc()
 
 # generate derived plots
-source("4A_compare_ongoing_and_responsive.R")
-source("4B_compare_ongoing_and_responsive.R")
-source("4C_compare_ongoing_and_responsive.R")
-source("5A_generate_patchwork_plots_for_ms.R")
-source("5B_generate_patchwork_plots.R")
-source("5C_generate_patchwork_plots.R")
+source("epi_plotting_functions/4A_compare_ongoing_and_responsive.R")
+source("epi_plotting_functions/4B_compare_ongoing_and_responsive.R")
+source("epi_plotting_functions/4C_compare_ongoing_and_responsive.R")
+source("epi_plotting_functions/5A_generate_patchwork_plots_for_ms.R")
+source("epi_plotting_functions/5B_generate_patchwork_plots.R")
+source("epi_plotting_functions/5C_generate_patchwork_plots.R")
 
-source("7_create_combination_plots.R")
+source("epi_plotting_functions/7_create_combination_plots.R")
 
 
