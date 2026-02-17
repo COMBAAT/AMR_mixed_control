d58fbdb3a0aa2991dc455ed44fc463a0db3fb7a0
LouiseMatthews <louise.matthews@glasgow.ac.uk>
2026-02-17 21:10:05 +0000

    streamlinging of plot_type20 to plot_type23

--- funcs/compare_responsive_and_ongoing_helper.R
+++ funcs/compare_responsive_and_ongoing_helper.R
@@ -3,26 +3,21 @@ plot_type20 <- function(df, x_var, y_var, this_prop_insecticide) {
   df$x <- df[, x_var]
   df$y <- df[, y_var]
   
-  df2 <- df %>% mutate(treatment_type = 
-                         case_when(treatment_type == "proph_ongoing" ~ "Ongoing longlasting", 
-                                   treatment_type == "curative" ~ "Responsive curative",
-                                   treatment_type == "longlasting" ~ "Responsive longlasting",
-                                   T ~ treatment_type))
+  df2 <- df 
+  
   p <- df2 %>% filter(prop_cattle_with_insecticide %in% c(this_prop_insecticide)) %>%
     ggplot() +
     geom_point(aes(y = y, x = x,
-                   #group = interaction(treatment_type, Baseline_vector_host_ratio),
-                   shape = treatment_type,
-                   colour = treatment_type), size = 1.5 * my_pointsize()) +
+                   shape = laXbel,
+                   colour = laXbel), size = 1.5 * my_pointsize()) +
     geom_line(aes(y = y, x = x, 
-                  #group = interaction(treatment_type, Baseline_vector_host_ratio), 
-                  colour = treatment_type), 
+                  colour = laXbel), 
               linewidth = my_linewidth()) +
     xlab(my_label(x_var)) +
     ylab(my_label(y_var)) +
     xlim(c(0, 1)) +
-    labs(colour = my_label("treatment_type", split_across_lines = "other"), 
-         shape = my_label("treatment_type", split_across_lines = "other")) +
+    labs(colour = my_label("laXbel", split_across_lines = "other"), 
+         shape = my_label("laXbel", split_across_lines = "other")) +
     scale_shape_manual(values = c(4, 19, 1)) +
     scale_x_continuous(breaks = seq(0, xmax_function(x_var), by = xmax_function(x_var)/4)) +
     facet_wrap(~ NW) +
@@ -43,16 +38,16 @@ plot_type21 <- function(df, x_var, y_var) {
   p <- df %>% filter(prop_cattle_with_insecticide %in% c(0, 0.05, 0.1, 0.2)) %>%
     ggplot() + 
     geom_point(aes(y = y, x = x, 
-                   group = interaction(treatment_type, Baseline_vector_host_ratio), 
-                   shape = treatment_type, 
-                   colour = treatment_type), size = 1.0 * my_pointsize()) +
+                   group = interaction(laXbel, Baseline_vector_host_ratio), 
+                   shape = laXbel, 
+                   colour = laXbel), size = 1.0 * my_pointsize()) +
     geom_line(aes(y = y, x = x, 
-                  group = interaction(treatment_type, Baseline_vector_host_ratio), 
-                  colour = treatment_type), linewidth = my_linewidth()) +
+                  group = interaction(laXbel, Baseline_vector_host_ratio), 
+                  colour = laXbel), linewidth = my_linewidth()) +
     xlab(my_label(x_var)) +
     ylab(my_label(y_var)) +
     xlim(c(0, 1)) +
-    labs(colour = my_label("treatment_type"), shape = my_label("treatment_type")) +
+    labs(colour = my_label("laXbel"), shape = my_label("laXbel")) +
     scale_shape_manual(values = c(4, 19, 1)) +
     facet_wrap(~ NW + prop_cattle_with_insecticide) +
     scale_x_continuous(breaks = seq(0, xmax_function(x_var), by = xmax_function(x_var)/4)) +
@@ -76,18 +71,11 @@ plot_type22 <- function(df, y_var, this_NW_set, this_vector_measure, ttype) {
   this_xlab <- my_label(x_var, split_across_lines = "default")
   this_ylab <- my_label(y_var, split_across_lines = "other")
   df$shape_variable <- df[, this_vector_measure]
-  #y_max <- my_lim(y_var)
   
   p <- df %>%
-    mutate_at(c("prop_cattle_with_insecticide", "NW", "shape_variable"), as.factor) %>%
-    filter(
-      prop_cattle_with_insecticide == 0,
-      NW %in% this_NW_set
-    ) %>%
     ggplot(aes(x, y, colour = shape_variable)) +
     geom_point(size = my_pointsize()) +
     geom_line(linewidth = my_linewidth()) +
-    #ylim(c(0, y_max)) +
     xlab(this_xlab) +
     ylab(this_ylab) +
     labs(colour = my_label(this_vector_measure)) +
@@ -98,8 +86,7 @@ plot_type22 <- function(df, y_var, this_NW_set, this_vector_measure, ttype) {
 }
 
 plot_type23 <- function(df, y_var, this_vector_measure, 
-                                                                 this_vector_measure_value,
-                                                                 this_NW_set, ttype) {
+                        this_vector_measure_value, this_NW_set, ttype) {
   
   x_var <- get_treat_var(df, ttype) 
   ymax <- ymax_function(y_var)
