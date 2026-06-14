a73e8ddd4cb7c69be83e4db12265d922e8245aeb
Louise Matthews <louise.matthews@glasgow.ac.uk>
2026-06-14 14:42:39 +0100

    updates to script names

--- epi_plotting_functions/8_generate_epi_plots.R
+++ epi_plotting_functions/8_generate_epi_plots.R
@@ -5,13 +5,26 @@ source("epi_plotting_functions/3_plot_explore_test.R")
 toc()
 
 # generate derived plots
-source("epi_plotting_functions/4A_compare_ongoing_and_responsive.R")
-source("epi_plotting_functions/4B_compare_ongoing_and_responsive.R")
-source("epi_plotting_functions/4C_compare_ongoing_and_responsive.R")
-source("epi_plotting_functions/5A_generate_patchwork_plots_for_ms.R")
+source("epi_plotting_functions/4A_compare_outcomes_by_protocol_and_wildlife.R")
+source("epi_plotting_functions/4B_compare_outcomes_by_protocol_and_vector_host_ratio.R")
+source("epi_plotting_functions/4C_compute_maximum_safe_treatment_intensity.R")
+source("epi_plotting_functions/4E_plot_helper.R")
+source("epi_plotting_functions/4E_generate_threshold.R")
+
+# all currently spare in G and H
+source("epi_plotting_functions/4G_plot_helper.R")
+source("epi_plotting_functions/4G_outcome_versus_prevalence.R")
+source("epi_plotting_functions/4H_outcomes_versus_prevalence_drug_only.R")
+#
+
+source("epi_plotting_functions/5A_generate_patchwork_plots.R")
 source("epi_plotting_functions/5B_generate_patchwork_plots.R")
 source("epi_plotting_functions/5C_generate_patchwork_plots.R")
+source("epi_plotting_functions/5D_generate_patchwork_plots.R")
+source("epi_plotting_functions/5E_generate_patchwork_plots.R")
 
 source("epi_plotting_functions/7_create_combination_plots.R")
 
-source("epi_plotting_functions/4D_generate_panel.R")
+
+
+
